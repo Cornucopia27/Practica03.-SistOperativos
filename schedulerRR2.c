63ab9f99b44e74b405d66f1e0ec85b6df999de7e
Cornucopia27 <ie703121@iteso.mx>
2018-09-20 21:13:19 -0600

    RR done

---
+++ schedulerRR2.c
@@ -0,0 +1,74 @@
+#include <scheduler.h>
+
+extern THANDLER threads[MAXTHREAD];
+extern int currthread;
+extern int blockevent;
+extern int unblockevent;
+
+#define TIME_SLICE 2
+
+QUEUE ready;
+QUEUE waitinginevent[MAXTHREAD];
+
+int time_slice = 0;
+
+void scheduler(int arguments)
+{
+	int old,next;
+	int changethread=0;
+	int waitingthread=0;
+
+	int event=arguments & 0xFF00;
+	int callingthread=arguments & 0xFF;
+
+
+
+	if(event==NEWTHREAD)
+	{
+		// Un nuevo hilo va a la cola de listos
+		threads[callingthread].status=READY;
+		_enqueue(&ready,callingthread);
+	}
+
+	if(event==BLOCKTHREAD)
+	{
+
+		threads[callingthread].status=BLOCKED;
+		_enqueue(&waitinginevent[blockevent],callingthread);
+
+		changethread=1;
+	}
+
+	if(event==ENDTHREAD)
+	{
+		threads[callingthread].status=END;
+		changethread=1;
+	}
+
+	if(event==UNBLOCKTHREAD)
+	{
+			threads[callingthread].status=READY;
+			_enqueue(&ready,callingthread);
+	}
+
+	if(event==TIMER)
+	{
+			time_slice++;
+			if(time_slice==TIME_SLICE)
+			{
+				_enqueue(&ready,callingthread);
+					changethread=1;
+					time_slice = 0;
+			}
+	}
+
+	if(changethread)
+	{
+		old=currthread;
+		next=_dequeue(&ready);
+
+		threads[next].status=RUNNING;
+		_swapthreads(old,next);
+	}
+
+}
